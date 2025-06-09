4d439b91f9baffb5bce88a458736a316c93b2fb1
Daniel Dieguez <dieguezd91@gmail.com>
2025-06-08 22:37:18 -0300

    Portal

--- Ninja Slasher/Assets/Scripts/View.cs
+++ Ninja Slasher/Assets/Scripts/View.cs
@@ -5,5 +5,7 @@ public class View : MonoBehaviour
     public Rigidbody2D RB => _rb;
     [SerializeField] Rigidbody2D _rb;
 
+    public Portal LastUsedPortal { get; set; }
+
     public Vector2 CurrentVelocity;
 }
