304686ea18bf4c1278c7ba1aeb0260ff54a0f12c
Tomas Taboada <tomastaboada27@gmail.com>
2025-06-22 14:56:15 -0300

    feature/ OM3GA

--- Ninja Slasher/Assets/Scripts/Enemies/OM3GA.cs
+++ Ninja Slasher/Assets/Scripts/Enemies/OM3GA.cs
@@ -1,27 +1,45 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class OM3GA : RangeEnemy
 {
+    [Header("Laser")]
     [SerializeField] float _rayDuration;
+    [SerializeField] float _laserDuration;
+    [SerializeField] float _timeToShoot;
+    [SerializeField] float _laserLength;
+    [SerializeField] private LayerMask _playerLayer;
+
+    [Header("Patrolling")]
     [SerializeField] Transform[] _nodes;
     [SerializeField] float _speed;
 
-    private bool _isAttacking;
-    private Vector2 _patrolTarget;
-
     private float _direction => transform.localScale.x > 0 ? 1 : -1;
+    private bool _isAttacking;
+    private bool _isShooting;
+    private Vector2 _patrolTarget;
+    private LineRenderer _ray;
+
 
     public override void Awake()
     {
         base.Awake();
         SetPatrolTarget();
+        TryGetComponent(out LineRenderer ray);
+        _ray = ray;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+        _ray.enabled = false;
     }
     public override void Update()
     {
         UpdateTarget();
         _isAttacking = _hasLOS;
-        if(_isAttacking)
+        if(_isAttacking || _isShooting)
         {
             _rb.linearVelocity = Vector2.zero;
             TryAttack();
@@ -55,21 +73,54 @@ public class OM3GA : RangeEnemy
         return Vector2.Distance(target, transform.localToWorldMatrix.GetPosition()) <= .5f;
     }
 
-    //public override void Attack()
-    //{
-    //    RaycastHit2D hit = Physics2D.Raycast(_refPoint.position, _dirToTarget.normalized, 10, _player.gameObject.layer);
-    //    Debug.DrawLine(_refPoint.position, (Vector2) _refPoint.position + _dirToTarget.normalized * hit.distance, Color.cyan, _rayDuration);
+    public override void Attack()
+    {
+        SetLastAttack();
+        StartCoroutine(ShootLaser());
+    }
 
-    //    if(hit && hit.collider.CompareTag("Player"))
-    //    {
-    //        hit.collider.TryGetComponent(out Controller player);
-    //        player.Die();
-    //    }
-    //    else if (!hit)
-    //    {
-    //        Debug.Log("Algo anda mal...");
-    //    }
-    //}
+    private IEnumerator ShootLaser()
+    {
+        if (_player != null)
+        {
+            _isShooting = true;
+            yield return new WaitForSeconds(_timeToShoot/2);
+            Vector2 dir = (_player.position - _refPoint.position).normalized;
+
+            yield return new WaitForSeconds(_timeToShoot/2);
+
+            _refPoint.right = dir;
+
+            if (_ray != null)
+            {
+                _ray.enabled = true;
+                _ray.SetPosition(0, _refPoint.position);
+
+                RaycastHit2D hit = Physics2D.Raycast(_refPoint.position, dir, _laserLength, _playerLayer);
+                Vector3 endPoint = _refPoint.position + (Vector3)dir * _laserLength;
+
+                if (hit.collider != null)
+                {
+                    endPoint = hit.point;
+                    if (hit.collider.CompareTag("Player"))
+                    {
+                        Controller playerController = hit.collider.GetComponent<Controller>();
+                        if (playerController != null)
+                        {
+                            playerController.Die();
+                        }
+                    }
+                }
+
+                _ray.SetPosition(1, endPoint);
+            }
+        }
+
+        yield return new WaitForSeconds(_laserDuration);
+
+        _ray.enabled = false;
+        _isShooting = false;
+    }
 
     private void SetPatrolTarget()
     {
