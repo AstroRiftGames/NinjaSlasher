1092226363c1f03d0e872d9cb1af7cd17c50a1df
Tomas Taboada <tomastaboada27@gmail.com>
2025-07-21 16:13:59 -0300

    Add FSM and decision tree framework scripts
    Introduced a finite state machine (FSM) implementation with generic state and transition support, including IState and State classes. Added a decision tree framework with ITreeNode, ActionNode, and QuestionNode for AI logic. Also included StateMono for MonoBehaviour-based states. These additions provide a foundation for advanced AI behaviors in the project.

---
+++ Ninja Slasher/Assets/Scripts/Stuff/FSM.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FSM<T>
+{
+    IState<T> _current;
+    public IState<T> CurrentState => _current;
+    public FSM(IState<T> initState)
+    {
+        SetInitalState(initState);
+    }
+    public void SetInitalState(IState<T> initState)
+    {
+        _current = initState;
+        _current.SetFSM = this;
+        _current.Enter();
+    }
+    public void OnUpdate()
+    {
+        if (_current != null)
+            _current.Execute();
+    }
+
+    public void Transition(T input)
+    {
+        IState<T> newState = _current.GetTransition(input);
+        if (newState != null)
+        {
+            _current.Sleep();
+            _current = newState;
+            _current.SetFSM = this;
+            _current.Enter();
+        }
+    }
+}
