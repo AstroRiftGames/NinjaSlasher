881e6856ac30ffe6ab61fd434229a46fcd75699b
Tomas Taboada <tomastaboada27@gmail.com>
2025-08-09 16:08:43 -0300

    Add TimeScale Management to GODMENU

--- Ninja Slasher/Assets/Scripts/Stuff/GodMenu.cs
+++ Ninja Slasher/Assets/Scripts/Stuff/GodMenu.cs
@@ -10,9 +10,16 @@ public class GodMenu : MonoBehaviour
     [SerializeField] Button _openCloseBTN;
     [SerializeField] Button _restartBTN;
     [SerializeField] Toggle _invincibleTGL;
+
+    //Enemy Spawn Management
     [SerializeField] TMP_Dropdown _enemiesDPD;
     [SerializeField] Button _spawnEnemyBTN;
     [SerializeField] Button _removeEnemiesBTN;
+    
+    //Time Scale Management
+    [SerializeField] Button _increaseScaleBTN;
+    [SerializeField] Button _decreaseScaleBTN;
+    [SerializeField] TextMeshProUGUI _currentScale;
 
     private bool _isOpen = false;
     [Header("Player")]
@@ -26,9 +33,13 @@ public class GodMenu : MonoBehaviour
     [SerializeField] Transform _enemiesInitial;
     [SerializeField] GameObject[] _enemiesPrefab;
 
+    [Space]
     List<TMP_Dropdown.OptionData> _enemyOptions = new List<TMP_Dropdown.OptionData>();
     GameObject _selectedEnemy;
     List<GameObject> _currentEnemies = new List<GameObject>();
+
+    [Header("Time Scale Management")]
+    [SerializeField] private float _scaleJump;
 
 
     private void Awake()
@@ -53,6 +64,8 @@ public class GodMenu : MonoBehaviour
         _enemiesDPD.onValueChanged.AddListener(SelectEnemy);
         _spawnEnemyBTN.onClick.AddListener(SpawnEnemy);
         _removeEnemiesBTN.onClick.AddListener(DestroyEnemies);
+        _increaseScaleBTN.onClick.AddListener(IncreaseScale);
+        _decreaseScaleBTN.onClick.AddListener(DecreaseScale);
     }
 
     private void OnDisable()
@@ -64,10 +77,18 @@ public class GodMenu : MonoBehaviour
         _enemiesDPD.onValueChanged.RemoveListener(SelectEnemy);
         _spawnEnemyBTN.onClick.RemoveListener(SpawnEnemy);
         _removeEnemiesBTN.onClick.RemoveListener(DestroyEnemies);
+        _increaseScaleBTN.onClick.RemoveListener(IncreaseScale);
+        _decreaseScaleBTN.onClick.RemoveListener(DecreaseScale);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) OpenClose();
     }
 
     private void OpenClose()
     {
+        Debug.Log("Open/Close");
         _isOpen = !_isOpen;
         _menuCanvasANIM.SetTrigger(_isOpen ? "Open" : "Close");
     }
@@ -103,5 +124,16 @@ public class GodMenu : MonoBehaviour
 
         _player = Instantiate(_playerPrefab, _playerInitialPos, Quaternion.identity).GetComponent<Controller>();
         Player = _player.transform;
+    }
+
+    private void IncreaseScale()
+    {
+        Time.timeScale += _scaleJump;
+        _currentScale.text = $"x{Time.timeScale}";
+    }
+    private void DecreaseScale()
+    {
+        Time.timeScale -= _scaleJump;
+        _currentScale.text = $"x{Time.timeScale}";
     }
 }
