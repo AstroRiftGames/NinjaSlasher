deb81689498e9b4e42ac973233700ebb873148b7
Tomas Taboada <tomastaboada27@gmail.com>
2025-08-17 18:37:27 -0300

    Fix a minor bug
    - Energy Projectile now increases its speed only when parried by the player

--- Ninja Slasher/Assets/Scripts/Projectiles/EnergyProjectile.cs
+++ Ninja Slasher/Assets/Scripts/Projectiles/EnergyProjectile.cs
@@ -5,7 +5,7 @@ public class EnergyProjectile : Projectile
     [SerializeField] [Range(1, 5)] float _multiplier;
     public override void ReflectBackwards(Transform newShooter, Vector2 newDir)
     {
-        MultiplySpeed(_multiplier);
+        if(newShooter.CompareTag("Player")) MultiplySpeed(_multiplier);
         base.ReflectBackwards(newShooter, newDir);
     }
 }
