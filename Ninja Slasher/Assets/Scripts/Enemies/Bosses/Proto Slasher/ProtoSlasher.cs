dae9de8bcb511e74bbfae546630a6fe0e4d97762
Tomas Taboada <tomastaboada27@gmail.com>
2025-08-17 18:33:39 -0300

    Implement parry, stun, and vulnerability for ProtoSlasher
    Added attack management, parry, stun, and vulnerability logic to ProtoSlasher boss. Updated player Controller to support enemy layer detection and improved projectile reflection targeting. Modified Player prefab and View to expose collider reference for collision handling.

--- Ninja Slasher/Assets/Scripts/Enemies/Bosses/Proto Slasher/ProtoSlasher.cs
+++ Ninja Slasher/Assets/Scripts/Enemies/Bosses/Proto Slasher/ProtoSlasher.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.InputSystem.XR;
 
+
+enum AttackEnum
+{
+    Burst,
+    Energy,
+    Dash,
+}
 public class ProtoSlasher : BossEnemy
 {
     [SerializeField] Transform _refPoint;
     [SerializeField] Transform _body;
-    private bool _isShooting;
-    private bool _isDashing;
     private Vector2 _dirToPlayer;
 
+    [Header("Attack Management")]
+    [SerializeField] float _attackCD;
+    private float _lastAttack;
+    private bool _isShooting;
+    private bool _isDashing;
+
     [Header("Dash")]
+    [SerializeField] [Range(0f, 1f)] float _dashChance;
     [SerializeField] float _dashForce;
     [SerializeField] float _timeBetweenDashes;
     [SerializeField] int _dashesAmount;
@@ -19,12 +30,23 @@ public class ProtoSlasher : BossEnemy
     [SerializeField] int _projectilesAmount;
     [SerializeField] float _timeBetweenShots;
     [SerializeField] GameObject _burstProjectilePrefab;
-    
+    [SerializeField] int _burstsToEnergy;
+    private int _burstsShot;
+
     [Header("Energy Shot")]
     [SerializeField] GameObject _energyProjectilePrefab;
     [SerializeField] float _chargingTime;
-    [SerializeField] float _inputChargeTime;
-    private float _inputTime;
+
+    [Header("Parry")]
+    [SerializeField] int _maxEnergyParries;
+    private int _energyParries;
+
+    [Header("Vulnerability")]
+    [SerializeField] CircleCollider2D _parryZone;
+    bool _isStunned;
+    [SerializeField] float _stunTime;
+    bool _isVulnerable;
+    [SerializeField] float _vulnerableTime;
 
     private void Update()
     {
@@ -35,24 +57,43 @@ public class ProtoSlasher : BossEnemy
         }
         else if(!_isDashing)
         {
-            if(Input.GetKeyDown(KeyCode.Space)) StartCoroutine(ManageDashes());
-            if (Input.GetKeyDown(KeyCode.G)) _inputTime = Time.time;
-            if(Input.GetKeyUp(KeyCode.G))
-            {
-                if(Time.time >= _inputTime + _inputChargeTime)
-                {
-                    StartCoroutine(ManageEnergyShot());
-                }
-                else
-                {
-                    StartCoroutine(ManageBurst());
-                }
-            }
+            TryAttack();
         }
     }
 
 
     #region ATTACK MANAGEMENT
+
+    private void TryAttack()
+    {
+        if(Time.time >= _lastAttack + _attackCD) 
+        {
+            switch (ChooseAttack())
+            {
+                case AttackEnum.Dash:
+                    StartCoroutine(ManageDashes());
+                    break;
+                case AttackEnum.Burst:
+                    StartCoroutine(ManageBurst());
+                    break;
+                case AttackEnum.Energy:
+                    StartCoroutine(ManageEnergyShot());
+                    break;
+            }
+            _lastAttack = Time.time;
+        }
+    }
+    private AttackEnum ChooseAttack()
+    {
+        if (_burstsShot >= _burstsToEnergy) return AttackEnum.Energy;
+        else
+        {
+            float r = Random.Range(0f, 1f);
+            if (r <= _dashChance) return AttackEnum.Dash;
+            else return AttackEnum.Burst;
+        }
+    }
+
     private IEnumerator ManageDashes()
     {
         _isDashing = true;
@@ -66,6 +107,7 @@ public class ProtoSlasher : BossEnemy
     private IEnumerator ManageBurst()
     {
         _isShooting = true;
+        _burstsShot++;
         for (int n = 0; n < _projectilesAmount; n++) 
         {
             Shoot(_burstProjectilePrefab);
@@ -78,6 +120,8 @@ public class ProtoSlasher : BossEnemy
         _isShooting = true;
         yield return new WaitForSeconds(_chargingTime);
         Shoot(_energyProjectilePrefab);
+        _energyParries = 0;
+        _burstsShot = 0;
         _isShooting = false;
     }
     #endregion
@@ -107,17 +151,94 @@ public class ProtoSlasher : BossEnemy
 
     #region PARRYING
 
+    private void TryParry(bool isEnergyProj)
+    {
+        if(isEnergyProj)
+        {
+            _energyParries++;
+            if (_energyParries >= _maxEnergyParries)
+            {
+                StartCoroutine(SetStunned());
+            }
+        }
+        StartParry();
+    }
+
+    private void StartParry()
+    {
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, 2f, LayerMask.GetMask("Projectiles"));
+
+        foreach (var hit in hits)
+        {
+            Projectile proj = hit.GetComponent<Projectile>();
+            if (proj != null && proj.IsParryable)
+            {
+                proj.ReflectBackwards(transform, GetDirToPlayer());
+            }
+        }
+    }
+    #endregion
+
+    #region STUN/VULNERABILITY MANAGEMENT
+    private IEnumerator SetStunned()
+    {
+        _isStunned = true;
+        _parryZone.enabled = false;
+        yield return new WaitForSeconds(_stunTime);
+        _energyParries = 0;
+        _parryZone.enabled = true;
+        _isStunned = false;
+    }
+
+    private IEnumerator SetVulnerable()
+    {
+        _isVulnerable = true;
+        yield return new WaitForSeconds(_vulnerableTime);
+        _isVulnerable = false;
+
+    }
     #endregion
 
     #region RESOURCES
     private Vector2 GetDirToPlayer() => (_player.position - transform.position).normalized;
     #endregion
+
+    #region COLLISIONS/TRIGGERS MANAGEMENT
     private void OnCollisionEnter2D(Collision2D collision)
     {
         string colTag = collision.gameObject.tag;
-        if (colTag is "Ceiling" or "Scenario" or "Floor" or "Platform")
+        switch(colTag)
         {
-            _rb.linearVelocity = Vector2.zero;
+            case "Ceiling" or "Scenario" or "Floor" or "Platform":
+                _rb.linearVelocity = Vector2.zero;
+                break;
+            case "Projectile":
+                collision.gameObject.TryGetComponent(out Projectile projectile);
+                if (projectile.Shooter.CompareTag("Player"))
+                {
+                    if (!_isStunned)
+                    {
+                        TryParry(collision.gameObject.name.Contains("Energy"));
+                    }
+                    else
+                    {
+                        StartCoroutine(SetVulnerable());
+                        Destroy(projectile.gameObject);
+                    }
+                }
+                break;
+            case "Player":
+                if(!_isVulnerable)
+                {
+                    collision.gameObject.TryGetComponent(out Controller player);
+                    player.Die();
+                }
+                else
+                {
+                    Die();
+                }
+                break;
         }
     }
+    #endregion
 }
