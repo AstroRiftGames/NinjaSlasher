806930889aef0d57b17ff56cb180f61b903da712
Tomas Taboada <tomastaboada27@gmail.com>
2025-08-17 18:33:15 -0300

    Refactor projectile reflection and parry logic
    Updated projectile prefabs to use the 'Projectile' tag and set colliders to non-trigger for improved collision handling. Refactored Projectile and EnergyProjectile scripts to simplify reflection and parry logic, removing reflected speed and parry count, and standardizing reflection to use shooter and direction. Updated TagManager to add 'Projectile' tag. Adjusted sorting layers and parameters for several projectile prefabs.

--- Ninja Slasher/Assets/Scripts/Projectiles/EnergyProjectile.cs
+++ Ninja Slasher/Assets/Scripts/Projectiles/EnergyProjectile.cs
@@ -2,19 +2,10 @@ using UnityEngine;
 
 public class EnergyProjectile : Projectile
 {
-    int _parriedTimes;
-    [SerializeField] int _maxParries;
     [SerializeField] [Range(1, 5)] float _multiplier;
-    public override void ReflectBackwards()
+    public override void ReflectBackwards(Transform newShooter, Vector2 newDir)
     {
-        MultiplyReflectedSpeed(_multiplier);
-        base.ReflectBackwards();
-        _parriedTimes++;
-        if (_parriedTimes >= _maxParries)
-        {
-            SetIsParryable(false);
-            TryGetComponent(out SpriteRenderer sp);
-            sp.color = Color.red;
-        }
+        MultiplySpeed(_multiplier);
+        base.ReflectBackwards(newShooter, newDir);
     }
 }
