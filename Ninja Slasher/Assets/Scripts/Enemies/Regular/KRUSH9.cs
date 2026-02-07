248eb82e4e6e121edac118414304a9402b2854e5
Tomas Taboada <tomastaboada27@gmail.com>
2026-02-07 19:03:10 -0300

    KRUSH9: add attack logic, animator params & prefab tweaks
    Enable KRUSH-9 attack behavior and wire up animator parameters. Changes include: script - add _attackCD, _lastAttack, Attack(), TargetClose(), CanAttack(), OnDrawGizmos(), and use AttackSpeed animator float/trigger; animation - adjust attack clip keyframes/curves; animator controller - add OnAttack/OnHit/AttackSpeed parameters and transitions for attack/hit states; prefabs - reposition/scale VFX and body parts, set Enemy tag and layer 9, add colliders/Rigidbody/MonoBehaviours for hitboxes, and minor serialized field additions; scriptable object - set _range to 5. These updates connect animation, controller and prefabs to support timed melee attacks.

--- Ninja Slasher/Assets/Scripts/Enemies/Regular/KRUSH9.cs
+++ Ninja Slasher/Assets/Scripts/Enemies/Regular/KRUSH9.cs
@@ -5,6 +5,8 @@ using AstroRift.Core.Update;
 public class KRUSH9 : Enemy
 {
     [SerializeField] float _attackSpeed;
+    [SerializeField] float _attackCD;
+    float _lastAttack;
 
     public override void Awake()
     {
@@ -15,11 +17,37 @@ public class KRUSH9 : Enemy
     public override void Start()
     {
         base.Start();
-        _animator.SetFloat("Speed", _attackSpeed);
+        _animator.SetFloat("AttackSpeed", _attackSpeed);
     }
 
     public override void CustomUpdate()
     {
-        _animator.SetFloat("Speed", _attackSpeed);
+        _animator.SetFloat("AttackSpeed", _attackSpeed);
+        if(TargetClose() && CanAttack())
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        _animator.SetTrigger("OnAttack");
+        _lastAttack = Time.time;
+    }
+
+    private bool TargetClose()
+    {
+        return Vector2.Distance(transform.position, _player.position) < _data.Range;
+    }
+
+    private bool CanAttack()
+    {
+        return Time.time > _lastAttack + _attackCD;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _data.Range);
     }
 }
