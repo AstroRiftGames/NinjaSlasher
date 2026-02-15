3567f3f757ac2277cb01a55fafcc3b395dcd2edb
Tomas Taboada <tomastaboada27@gmail.com>
2026-02-15 17:37:59 -0300

    Start Arachnomadre Rework
    Create NewArachnomadre.cs. Import "crawler" movement system from BL4ZT.cs, adapt system to Arachnomadre's needs.

---
+++ Ninja Slasher/Assets/Scripts/Enemies/Bosses/Arachnomadre BL-KR/NewArachnomadre.cs
@@ -0,0 +1,348 @@
+using System.Collections;
+using UnityEngine;
+
+public class NewArachnomadre : BossEnemy
+{
+
+    #region VARIABLES
+    //EXTRAS
+    [SerializeField] Transform _spriteContainer;
+    [SerializeField] private float _verticalOffset = 1f;
+    [SerializeField] private float _horizontalOffset = 1f;
+    [SerializeField] private float groundCheckOffset = 0.15f;
+    [SerializeField] private float groundCheckDistance = 0.05f;
+
+    [Header("Movement")]
+    [SerializeField] private float wallCheckDistance = 0.2f;
+    [SerializeField] private float _speed;
+    private bool _goingRight = true;
+
+    [Header("Rotation")]
+    [SerializeField] private float rotationSpeed;
+    private float _pivotDistance = 0.05f;
+    private float turnSign;
+    private bool isTurning;
+    private float targetAngle;
+    private Vector2 pivotPoint;
+
+    [Header("Navigation")]
+    private Vector2 currentNormal = Vector2.up;
+    #endregion
+
+    #region SURFACE DETECTION
+    private bool DetectGround(float offset, out RaycastHit2D hit)
+    {
+        Vector3 origin =
+            transform.position +
+            GetMovementDir() * offset +
+            -transform.up * _verticalOffset;
+
+        Vector2 direction = -transform.up;
+
+        hit = Physics2D.Raycast(origin, direction, groundCheckDistance, _obstaclesLayer);
+        Debug.DrawRay(origin, direction * groundCheckDistance, Color.red);
+
+        return hit.collider != null;
+    }
+
+    private bool DetectWall(out RaycastHit2D hit)
+    {
+        Vector3 origin = transform.position + transform.up + transform.right * _horizontalOffset;
+        Vector2 direction = GetMovementDir();
+
+        hit = Physics2D.Raycast(origin, direction, wallCheckDistance, _obstaclesLayer);
+        Debug.DrawRay(origin, direction * wallCheckDistance, Color.red);
+
+        return hit.collider != null;
+    }
+
+    #endregion
+
+    #region MOVEMENT & ALIGNMENT
+    private bool HandleMovement(bool groundFront, RaycastHit2D frontHit, bool groundBack, RaycastHit2D backHit, bool wallAhead)
+    {
+        //ALIGNMENT
+        AlignToSurface(groundFront ? frontHit.normal : backHit.normal);
+
+        //ROTATION
+        if (CheckCorner(groundFront, groundBack, wallAhead))
+        {
+            return true;
+        }
+
+        //MOVEMENT
+        MoveAlongSurface();
+        SnapToSurface();
+        return false;
+    }
+
+    private bool CheckCorner(bool groundFront, bool groundBack, bool wallAhead)
+    {
+        // CLOSE CORNER
+        if (wallAhead && groundFront && groundBack)
+        {
+            Vector2 newNormal =
+                new Vector2(-currentNormal.y, currentNormal.x);
+
+            StartTurn(newNormal, true);
+            return true;
+        }
+
+        // OPEN CORNER
+        if (!wallAhead && groundBack && !groundFront)
+        {
+            Vector2 newNormal =
+                new Vector2(currentNormal.y, -currentNormal.x);
+
+            StartTurn(newNormal, false);
+            return true;
+        }
+
+        return false;
+    }
+
+    private void AlignToSurface(Vector2 normal)
+    {
+        currentNormal = normal;
+
+        Vector2 tangent = new Vector2(normal.y, -normal.x);
+
+        if (Vector2.Dot(tangent, transform.right) < 0)
+            tangent = -tangent;
+
+        float angle = Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void MoveAlongSurface()
+    {
+        transform.position += GetMovementDir() * _speed * Time.deltaTime;
+    }
+
+    private void SnapToSurface()
+    {
+        Vector3 origin =
+            transform.position -
+            transform.up * _verticalOffset;
+
+        Vector2 direction = -transform.up;
+
+        RaycastHit2D hit =
+            Physics2D.Raycast(origin, direction, groundCheckDistance, _obstaclesLayer);
+        Debug.DrawRay(origin, direction * groundCheckDistance, Color.blue);
+
+        if (!hit.collider)
+            return;
+
+        float delta = hit.distance;
+
+        transform.position -= transform.up * delta;
+    }
+    #endregion
+
+    #region ROTATION
+    private void StartTurn(Vector2 newNormal, bool isClosedCorner)
+    {
+        isTurning = true;
+        currentNormal = newNormal;
+
+        turnSign = _goingRight ? (isClosedCorner ? 1f : -1f) : (isClosedCorner ? -1f : 1f);
+
+        Vector2 tangent = _goingRight
+            ? new Vector2(newNormal.y, -newNormal.x)
+            : new Vector2(-newNormal.y, newNormal.x);
+
+        targetAngle = Mathf.Atan2(tangent.y, tangent.x) * Mathf.Rad2Deg;
+
+        pivotPoint = (Vector2)transform.position - (Vector2)transform.up * _pivotDistance;
+    }
+
+    private void UpdateTurn()
+    {
+        float current = transform.eulerAngles.z;
+
+        float diff = Mathf.DeltaAngle(current, targetAngle);
+
+        float step = rotationSpeed * Time.deltaTime * turnSign;
+
+        if (Mathf.Sign(diff) != Mathf.Sign(step) || Mathf.Abs(step) >= Mathf.Abs(diff))
+        {
+            step = diff;
+            isTurning = false;
+        }
+
+        transform.RotateAround(pivotPoint, Vector3.forward, step);
+
+        transform.position += GetMovementDir() * _speed * Time.deltaTime;
+
+        if (!isTurning)
+        {
+            SnapToSurface();
+        }
+    }
+    #endregion
+
+    #region AI
+    //private bool HasLOS()
+    //{
+    //    Vector2 dirToPlayer = (_player.transform.position - transform.position).normalized;
+    //    RaycastHit2D hit = Physics2D.Raycast(transform.position, dirToPlayer, _data.Range, _playerLayer);
+    //    return hit;
+
+    //}
+
+    //private bool CheckTarget(Vector3 target)
+    //{
+    //    bool hasReachedTarget = Vector3.Distance(target, transform.position) <= _explosionRadius / 2;
+    //    _animator.SetBool("IsMoving", !hasReachedTarget);
+    //    return hasReachedTarget;
+    //}
+
+    //private void SetDirToTarget()
+    //{
+    //    Vector3 localTargetPos = transform.InverseTransformPoint(_destination);
+
+    //    float threshold = 0.05f;
+
+    //    if (Mathf.Abs(localTargetPos.x) > threshold)
+    //    {
+    //        _goingRight = localTargetPos.x > 0;
+    //    }
+    //    Vector3 newScale = _spriteContainer.localScale;
+    //    newScale.x = _goingRight ? -Mathf.Abs(newScale.x) : Mathf.Abs(newScale.x);
+    //    _spriteContainer.localScale = newScale;
+    //}
+
+    //private IEnumerator SetPatrolTarget()
+    //{
+    //    _isWaiting = true;
+    //    _rb.linearVelocityX = 0;
+    //    _animator.SetBool("IsMoving", false);
+
+    //    yield return new WaitForSeconds(0.5f);
+    //    IncreaseNodeIndex();
+    //    Transform _nextNode = _nodes[_currentNodeIndex];
+
+    //    _destination = GetClosestPoint(_nextNode.position);
+    //    SetDirToTarget();
+
+    //    yield return new WaitForSeconds(.5f);
+    //    _animator.SetBool("IsMoving", true);
+    //    _isWaiting = false;
+    //}
+
+    //private Vector2 GetClosestPoint(Vector2 origin)
+    //{
+    //    _lastRay = Time.time;
+    //    Vector2 closestPoint = origin;
+    //    float disToClosestSurface = float.MaxValue;
+
+    //    for (int n = 0; n < 4; n++)
+    //    {
+    //        Vector2 dirToCast = GetDirectionByIndex(n);
+    //        RaycastHit2D hit = Physics2D.Raycast(origin, dirToCast, 15, base._obstaclesLayer);
+    //        float disToCurrent = Vector2.Distance(origin, hit.point);
+
+    //        if (disToClosestSurface == 0 || disToClosestSurface > disToCurrent)
+    //        {
+    //            disToClosestSurface = disToCurrent;
+    //            closestPoint = hit.point;
+    //        }
+    //    }
+    //    return closestPoint;
+    //}
+
+    #endregion
+
+    #region COMBAT
+    //private void Activate()
+    //{
+    //    _isActive = true;
+    //    _activationTime = Time.time;
+    //    if (CheckCooldown(_rayCD, _lastRay)) _destination = GetClosestPoint(_player.transform.position);
+    //    _currentSpeed *= _speedMultiplier;
+    //    _animator.SetTrigger("OnActivated");
+    //    _animator.SetBool("IsActive", true);
+    //}
+    //private void Explode()
+    //{
+    //    Collider2D[] cols = Physics2D.OverlapCircleAll(transform.position, _explosionRadius);
+    //    foreach (Collider2D col in cols)
+    //    {
+    //        if (col.CompareTag("Player"))
+    //        {
+    //            col.TryGetComponent(out NewController player);
+    //            player.Die();
+    //        }
+    //        else if (col.CompareTag("Enemy") && col.TryGetComponent(out Enemy enemy))
+    //        {
+    //            enemy.Die();
+    //        }
+    //        else if (col.TryGetComponent(out BreakableProp prop))
+    //        {
+    //            prop.Break();
+    //        }
+    //        else if (_arachnomadre != null && col.CompareTag("Boss"))
+    //        {
+    //            _arachnomadre.StartCoroutine(_arachnomadre.GetVulnerable());
+    //        }
+    //    }
+    //    Die();
+    //}
+
+    //private bool CheckExplosionTime()
+    //{
+    //    return Time.time >= _activationTime + _timeToExplode;
+    //}
+
+    //public override void Die()
+    //{
+    //    if (_arachnomadre != null)
+    //    {
+    //        _arachnomadre.DecreaseEggsAmount();
+    //    }
+    //    _animator.SetTrigger("OnHit");
+    //    base.Die();
+    //    Destroy(transform.parent.gameObject, .6f);
+    //}
+    #endregion
+
+    #region UTILS
+    private Vector3 GetMovementDir() => _goingRight ? transform.right : -transform.right;
+    
+    private bool CheckCooldown(float cd, float last) => Time.time >= cd + last;
+    #endregion
+
+    #region MAGIC METHODS
+    private void Update()
+    {
+        if (isTurning)
+        {
+            UpdateTurn();
+            return;
+        }
+
+        bool groundFront = DetectGround(groundCheckOffset, out RaycastHit2D frontHit);
+        bool groundBack = DetectGround(-groundCheckOffset, out RaycastHit2D backHit);
+        bool wallAhead = DetectWall(out RaycastHit2D wallHit);
+
+        Debug.Log("Ground Front: " + groundFront);
+        Debug.Log("Ground Back: " + groundBack);
+
+        if (!groundFront && !groundBack)
+        {
+            _animator.SetBool("IsMoving", false);
+            return;
+        }
+        else if(_animator.GetBool("IsMoving") == false)
+        {
+            _animator.SetBool("IsMoving", true);
+        }
+
+
+        //CHANGE
+        HandleMovement(groundFront, frontHit, groundBack, backHit, wallAhead);
+    }
+    #endregion
+}
+
