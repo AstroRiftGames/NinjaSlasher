3ef8bd9a78c8499b5ae4ed96fcf18f174a8adad2
Tomas Taboada <tomastaboada27@gmail.com>
2026-02-23 21:31:11 -0300

    & GuardBot

--- Ninja Slasher/Assets/Scripts/Enemies/GuardBotShield.cs
+++ Ninja Slasher/Assets/Scripts/Enemies/GuardBotShield.cs
@@ -3,19 +3,15 @@ using UnityEngine;
 public class GuardBotShield : VulnerabilityCheck
 {
     private GuardBot _bot;
-    private Collider2D _col;
 
     private void Awake()
     {
         _bot = GetComponentInParent<GuardBot>();
-        TryGetComponent(out Collider2D col);
-        _col = col;
     }
 
     public override void ManageColision()
     {
         base.ManageColision();
-        //AudioManager.Instance.PlaySFXAtPosition(SFXClip.E_Guard_Colision, _bot.transform.position);
         AudioService.Instance.PlaySFXAtPosition(_bot.AudioContext.Audio.collision, _bot.transform.position);
     }
 }
