7668369d051ab3fbf4662cf5ba0a2f1aa81e2ff0
Daniel Dieguez <dieguezd91@gmail.com>
2026-03-02 11:58:51 -0300

    Add LevelPlay sample and update plugins/assets
    Add LevelPlay Unity sample (scenes, scripts, UI, and .meta files) and include backups for Android mainTemplate.gradle. Update DOTween plugin (added UIToolkit module and various DLLs/metadata) and modify several game scenes, scripts, project settings and packages. Also include regenerated build artifacts and CMake/Ninja temporary files from Android builds.

--- Ninja Slasher/Assets/Scripts/Gameplay Systems/Audio System/AudioSettingsSO.cs
+++ Ninja Slasher/Assets/Scripts/Gameplay Systems/Audio System/AudioSettingsSO.cs
@@ -1,5 +1,4 @@
 using System;
-using UnityEditor.U2D;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "Game/Audio/Audio Settings")]
--- Ninja Slasher/Assets/Scripts/Stuff/GeyserDeathCol.cs
+++ Ninja Slasher/Assets/Scripts/Stuff/GeyserDeathCol.cs
@@ -1,6 +1,4 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class GeyserDeathCol : MonoBehaviour
 {
