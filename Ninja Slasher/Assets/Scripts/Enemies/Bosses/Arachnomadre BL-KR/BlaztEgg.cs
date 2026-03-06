0abbaf43c5f46e643ad1b2a9407b2b85db2f1d30
Tomas Taboada <tomastaboada27@gmail.com>
2026-03-06 20:08:54 -0300

    Add new BL4ZT Egg Sprites

--- Ninja Slasher/Assets/Scripts/Enemies/Bosses/Arachnomadre BL-KR/BlaztEgg.cs
+++ Ninja Slasher/Assets/Scripts/Enemies/Bosses/Arachnomadre BL-KR/BlaztEgg.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class BlaztEgg : MonoBehaviour, IPoolable
 {
     [SerializeField] GameObject BlaztPrefab;
+    [SerializeField] GameObject _regularEgg;
+    [SerializeField] GameObject _brokenEgg;
 
     private Arachnomadre _arachnomadre;
     public event Action<BlaztEgg> OnRequestDespawn;
 
     bool _hatched = false;
+    [SerializeField] float _despawnTime = 2.5f;
 
     public void SetArachnomadre(Arachnomadre boss) => _arachnomadre = boss;
 
@@ -30,6 +33,8 @@ public class BlaztEgg : MonoBehaviour, IPoolable
         if (!_hatched && collision.gameObject.CompareTag("Floor"))
         {
             _hatched = true;
+            _regularEgg.SetActive(false);
+            _brokenEgg.SetActive(true);
 
             BL4ZT newEnemy = Instantiate(
                 BlaztPrefab,
@@ -42,12 +47,13 @@ public class BlaztEgg : MonoBehaviour, IPoolable
             newEnemy.SetArachnomadre(_arachnomadre);
             _arachnomadre.IncreaseEggsAmount();
 
-            RequestDespawn();
+            StartCoroutine(RequestDespawn());
         }
     }
 
-    private void RequestDespawn()
+    private IEnumerator RequestDespawn()
     {
+        yield return new WaitForSeconds(_despawnTime);
         OnRequestDespawn?.Invoke(this);
     }
 }
