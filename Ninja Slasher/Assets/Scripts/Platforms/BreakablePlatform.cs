dc725d5b83a2ac7ebd911a1ba7edecf8bb9dcac9
Tomas Taboada <tomastaboada27@gmail.com>
2026-03-30 12:55:49 -0300

    Update Breakable Platfroms (Forest, Village & Cave)
    Changed Broken Parts System for a Partcile System with placeholder assets.
    Updated ninja's fall method after breaking.

--- Ninja Slasher/Assets/Scripts/Platforms/BreakablePlatform.cs
+++ Ninja Slasher/Assets/Scripts/Platforms/BreakablePlatform.cs
@@ -6,49 +6,36 @@ using UnityEngine.Tilemaps;
 public class BreakablePlatform : PlatformBase
 {
     [Header("SETTINGS")]
-    [SerializeField] private int maxUses;
-    [SerializeField] private float falloffVelocity;
-
-    private Rigidbody2D playerRb;
-    private int remainingUses;
-    [SerializeField] int _columns = 3;
-    [SerializeField] int _rows = 2;
-
-
+    [SerializeField] private int _maxUses;
+    private NewController _playerController;
+    private int _remainingUses;
 
     [SerializeField] GameObject _tilemap;
-    TilemapCollider2D _tilemapCol;
-    List<List<int>> pieces = new List<List<int>>();
-    Vector3[] _coords;
-    [Header("BREAKING")]
-    [SerializeField] GameObject[] _piecesPrefabs;
-    [SerializeField] float _explosionForce;
+    [SerializeField] ParticleSystem _particleSystem;
 
     public override void Awake()
     {
-        _tilemap.TryGetComponent(out TilemapCollider2D renderer);
-        _tilemapCol = renderer;
         base.Awake();
     }
 
 
     protected override void InitializePlatform()
     {
-        remainingUses = maxUses;
+        _remainingUses = _maxUses;
     }
 
     public override void OnPlayerExit(GameObject player, bool isForced = false) { }
 
     public override void OnPlayerEnter(GameObject player)
     {
-        player.TryGetComponent(out Rigidbody2D rb);
-        playerRb = rb;
+        player.TryGetComponent(out NewController controller);
+        _playerController = controller;
         if (!isActive) return;
 
 
-        remainingUses--;
+        _remainingUses--;
         AudioService.Instance.PlaySFXAtPosition(_audioContext.Audio.Interaction, transform.position);
-        if (remainingUses <= 0)
+        if (_remainingUses <= 0)
         {
             Break();
         }
@@ -63,8 +50,8 @@ public class BreakablePlatform : PlatformBase
         GameEvents.RaiseBreakablePlatformBroken();
         AudioService.Instance.PlaySFXAtPosition(_audioContext.Audio.DestroyPlatform, transform.position);
         DeactivateWhole();
-        GeneratePieces();
-        StartCoroutine(DestroyNextFrame());
+        _particleSystem.Play();
+        StartCoroutine(DestroyAfterParticles());
     }
 
     private void DeactivateWhole()
@@ -72,58 +59,10 @@ public class BreakablePlatform : PlatformBase
         _tilemap.SetActive(false);
     }
 
-    private void GeneratePieces()
-    {
-        CreateMatrix();
-        SetPositions();
-        SpawnPieces();
-    }
-
-    private void CreateMatrix()
-    {
-        _coords = new Vector3[_columns*_rows];
-        for (int c = 0; c < _columns; c++)
-        {
-            pieces.Add(new List<int>());
-            for (int r = 0; r < _rows; r++)
-            {
-                pieces[c].Add(r);
-            }
-        }
-    }
-    private void SetPositions()
-    {
-        Vector3 pos = Vector2.zero;
-        Vector3 offset = Vector2.zero;
-        int index = 0;
-        for (int c = 0; c < pieces.Count; c++)
-        {
-            for (int r = 0; r < pieces[c].Count; r++)
-            {
-                pos.x = c;
-                pos.y = r;
-                offset.x = c / 2;
-                offset.y = r / 2;
-                _coords[index] = transform.position + pos - offset;
-                index++;
-            }
-        }
-    }
-
-    private void SpawnPieces()
-    {
-        foreach (var coord in _coords)
-        {
-            GameObject randomPiece = _piecesPrefabs[Random.Range(0, _piecesPrefabs.Length)];
-            GameObject newPiece = Instantiate(randomPiece, coord, Quaternion.identity);
-            newPiece.TryGetComponent(out Rigidbody2D rb);
-            rb.AddForce((newPiece.transform.position - transform.position).normalized * _explosionForce, ForceMode2D.Impulse);
-        }
-    }
-    private IEnumerator DestroyNextFrame()
+    private IEnumerator DestroyAfterParticles()
     {
         yield return null;
-        playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, -falloffVelocity);
-        Destroy(gameObject);
+        _playerController.ForceDash(Vector2.down);
+        Destroy(gameObject, 1f);
     }
 }
