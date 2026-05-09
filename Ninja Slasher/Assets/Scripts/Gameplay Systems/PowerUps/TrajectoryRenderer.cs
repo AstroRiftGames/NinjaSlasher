818494466fe4be91cc7b330fba0ae2fa4ae04d38
Tomas Taboada <tomastaboada27@gmail.com>
2026-05-09 00:16:05 -0300

    Minor fixes

--- Ninja Slasher/Assets/Scripts/Gameplay Systems/PowerUps/TrajectoryRenderer.cs
+++ Ninja Slasher/Assets/Scripts/Gameplay Systems/PowerUps/TrajectoryRenderer.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class TrajectoryRenderer : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer _indicator;
-    public SpriteRenderer Indicator => _indicator;
+    //[SerializeField] private SpriteRenderer _indicator;
+    //public SpriteRenderer Indicator => _indicator;
 
     [SerializeField] private GameObject _hitMarker;
-    private ParticleSystem _hitParticles;
     private Animator _animator;
  
     [SerializeField] private LayerMask _collisionLayers;
-    [SerializeField] private float _defaultMaxDistance = 5f;
+    //[SerializeField] private float _defaultMaxDistance = 5f;
 
     [SerializeField] private float _playerRadius = 0.5f;
 
@@ -20,31 +19,24 @@ public class TrajectoryRenderer : MonoBehaviour
 
     void Awake()
     {
-        if (_indicator != null)
-        {
-            _fixedWidth = _indicator.size.x;
-            _indicator.enabled = false;
-        }
+        //if (_indicator != null)
+        //{
+        //    _fixedWidth = _indicator.size.x;
+        //    _indicator.enabled = false;
+        //}
 
         if (_hitMarker != null)
         {
             _hitMarker.SetActive(false);
-            _hitParticles = _hitMarker.GetComponent<ParticleSystem>();
             _animator = _hitMarker.GetComponent<Animator>();
-
-            if (_hitParticles != null)
-            {
-                _hitParticles.Stop();
-                _hitParticles.Clear();
-            }
         }
     }
 
     public void ShowTrajectory(Vector3 startPosition, Vector2 direction)
     {
-        if (_indicator == null || direction == Vector2.zero) return;
+        //if (_indicator == null || direction == Vector2.zero) return;
 
-        float maxDist = _hawkVisionSettings != null ? _hawkVisionSettings.maxDistance : _defaultMaxDistance;
+        //float maxDist = _hawkVisionSettings != null ? _hawkVisionSettings.maxDistance : _defaultMaxDistance;
 
         RaycastHit2D hit = CalculateHit(startPosition, direction);
 
@@ -67,31 +59,22 @@ public class TrajectoryRenderer : MonoBehaviour
                         _animator.SetBool("IsAvailable", isSafe);
                     }
                 }
-
-                if (_hitParticles != null && !_hitParticles.isPlaying)
-                {
-                    _hitParticles.Play();
-                }
             }
         }
         else
         {
-            distance = maxDist;
+            //distance = maxDist;
 
             if (_hitMarker != null)
             {
                 _hitMarker.SetActive(false);
-                if (_hitParticles != null && _hitParticles.isPlaying)
-                {
-                    _hitParticles.Stop();
-                }
             }
         }
 
-        UpdateArrowTransform(startPosition, direction, distance);
+        //UpdateArrowTransform(startPosition, direction, distance);
         UpdateHitMarkerRotation(hit.normal);
 
-        _indicator.enabled = true;
+        // _indicator.enabled = true;
     }
 
     private RaycastHit2D CalculateHit(Vector3 startPos, Vector2 direction)
@@ -105,16 +88,16 @@ public class TrajectoryRenderer : MonoBehaviour
         );
     }
 
-    private void UpdateArrowTransform(Vector3 startPos, Vector2 direction, float distance)
-    {
-        if (_indicator == null) return;
-        _indicator.transform.position = startPos;
+    //private void UpdateArrowTransform(Vector3 startPos, Vector2 direction, float distance)
+    //{
+    //    if (_indicator == null) return;
+    //    _indicator.transform.position = startPos;
 
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        _indicator.transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
+    //    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    //    _indicator.transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
         
-        _indicator.size = new Vector2(_fixedWidth, distance);
-    }
+    //    _indicator.size = new Vector2(_fixedWidth, distance);
+    //}
 
     private void UpdateHitMarkerRotation(Vector2 surfaceNormal)
     {
@@ -128,14 +111,10 @@ public class TrajectoryRenderer : MonoBehaviour
 
     public void HideTrajectory()
     {
-        if (_indicator != null) _indicator.enabled = false;
+        //if (_indicator != null) _indicator.enabled = false;
         if (_hitMarker != null) 
         {
             _hitMarker.SetActive(false);
-            if (_hitParticles != null && _hitParticles.isPlaying)
-            {
-                _hitParticles.Stop();
-            }
         }
     }
 }
